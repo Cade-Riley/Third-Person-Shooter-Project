6d0f7e3fbe0b6eb658acfab170fa27817db5ecb6
Mol-Dugaal <flubber9812@aol.com>
2022-08-12 04:13:38 -0400

    Title Screen
    -Added a title screen with working start and exit buttons
    -Added the target model for the menu and later use
    -Created scene for gallery with a placeholder model

---
+++ Assets/Scripts/MainMenuUiHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUiHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
